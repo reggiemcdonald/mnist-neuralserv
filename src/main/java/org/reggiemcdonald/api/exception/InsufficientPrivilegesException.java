1002be20b0818c50c7b57e50448a61d12763bd9d
reggiemcdonald <regmcdonald95@gmail.com>
2019-12-25 23:09:07 -0500

    Updated exceptions

--- src/main/java/org/reggiemcdonald/api/exception/InsufficientPrivilegesException.java
+++
@@ -1,7 +0,0 @@
-package org.reggiemcdonald.api.exception;
-
-public class InsufficientPrivilegesException extends Exception {
-    public InsufficientPrivilegesException() {
-        super("Insufficient privileges to access requested resource");
-    }
-}
--- src/main/java/org/reggiemcdonald/api/exception/NotFoundException.java
+++
@@ -1,7 +0,0 @@
-package org.reggiemcdonald.api.exception;
-
-public class NotFoundException extends Exception {
-    public NotFoundException(int id) {
-        super(String.format("Number with ID %d not found.", id));
-    }
-}
