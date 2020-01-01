0a04d15591150959279690feb90d15e1df5554bb
reggiemcdonald <regmcdonald95@gmail.com>
2020-01-01 13:53:57 -0500

    began implementing training endpoint

--- neuralserv-trainer/src/main/java/org/reggiemcdonald/api/TrainerController.java
+++ neuralserv-trainer/src/main/java/org/reggiemcdonald/api/TrainerController.java
@@ -1,4 +1,68 @@
 package org.reggiemcdonald.api;
 
+import com.reggiemcdonald.neural.feedforward.net.Network;
+import org.reggiemcdonald.api.model.TrainerControllerRequestModel;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.RestTemplate;
+
+import javax.annotation.PostConstruct;
+import javax.validation.Valid;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.concurrent.*;
+
+@RestController
+@RequestMapping("/trainer")
 public class TrainerController {
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    private ExecutorService executorService;
+    private RejectedExecutionHandler handler;
+
+    @PostConstruct
+    public void initialize() {
+        handler = new RejectedTrainingRequestHandler();
+        executorService =
+                new ThreadPoolExecutor(1, 10, 1, TimeUnit.MINUTES, new LinkedBlockingQueue<>(), handler);
+    }
+
+    @RequestMapping(value = "/train", method = RequestMethod.POST, consumes = "application/json")
+    public ResponseEntity<String> train(@Valid @RequestBody TrainerControllerRequestModel model) {
+        Network network;
+        Integer epochs = model.getEpochs();
+        Integer batchSize = model.getBatchSize();
+        Double eta = model.getEta();
+        Boolean verbose = model.getVerbose();
+        Boolean callback = model.getCallback();
+        String uri = model.getUrl();
+
+        try {
+            network = Network.loadWithException(TrainingRequest.NERL_FILE);
+        } catch (IOException e) {
+            network = new Network(new int[] {784, 100, 10});
+        }
+
+        TrainingRequest request =
+                new TrainingRequest(network, this, epochs, batchSize, eta, verbose, callback, uri);
+        executorService.submit(request);
+        return ResponseEntity.status(200).body("Began training network...");
+    }
+
+    public void onDone() {
+        System.out.println("DONE TRAINING");
+    }
+
+    public void onDoneRequest(String uri) {
+        try {
+            String response = restTemplate.postForObject(uri, null, String.class, new HashMap<>());
+            System.out.println(response);
+        } catch (HttpClientErrorException e) {
+            System.out.println(e.getMessage());
+        }
+    }
 }
---
+++ neuralserv-trainer/src/main/java/org/reggiemcdonald/api/TrainingRequest.java
@@ -0,0 +1,61 @@
+package org.reggiemcdonald.api;
+
+import com.reggiemcdonald.neural.feedforward.net.Network;
+import com.reggiemcdonald.neural.feedforward.res.ImageLoader;
+import com.reggiemcdonald.neural.feedforward.res.NumberImage;
+import org.apache.log4j.Logger;
+
+import java.nio.file.Paths;
+import java.util.List;
+
+public class TrainingRequest implements Runnable {
+    protected static final String NERL_FILE = "/Users/reginaldmcdonald/Library/Application Support/reggiemcdonald/mnist-neuralserv/network-state.nerl";
+    private static final String TRAIN_IMAGES = "/Users/reginaldmcdonald/Library/Application Support/reggiemcdonald/mnist-neuralserv/train_images.gz";
+    private static final String TRAIN_LABELS = "/Users/reginaldmcdonald/Library/Application Support/reggiemcdonald/mnist-neuralserv/train_labels.gz";
+    private static final String TEST_IMAGES = "/Users/reginaldmcdonald/Library/Application Support/reggiemcdonald/mnist-neuralserv/test_images.gz";
+    private static final String TEST_LABELS = "/Users/reginaldmcdonald/Library/Application Support/reggiemcdonald/mnist-neuralserv/test_labels.gz";
+
+    private Logger logger = Logger.getLogger(TrainingRequest.class);
+
+    private Network network;
+    private TrainerController controller;
+    private final int epochs;
+    private final int batchSize;
+    private final double eta;
+    private final boolean verbose;
+    private final boolean callback;
+    private final String uri;
+
+    public TrainingRequest(Network _network,
+                           TrainerController _controller,
+                           int _epochs,
+                           int _batchSize,
+                           double _eta,
+                           boolean _verbose,
+                           boolean _callback,
+                           String _uri) {
+        network = _network;
+        controller = _controller;
+        epochs = _epochs;
+        batchSize = _batchSize;
+        eta = _eta;
+        verbose = _verbose;
+        callback = _callback;
+        uri = _uri;
+    }
+
+    @Override
+    public void run() {
+        logger.info("Began running training protocol");
+//        List<NumberImage> trainingList = ImageLoader.load(Paths.get(TRAIN_IMAGES), Paths.get(TRAIN_LABELS));
+//        List<NumberImage> testingList = ImageLoader.load(Paths.get(TEST_IMAGES), Paths.get(TEST_LABELS));
+//        network.learn(trainingList,epochs, batchSize, eta, verbose);
+//        network.test(testingList);
+//        Network.save(network, NERL_FILE);
+        logger.info("Training protocol has finished executing");
+        if (callback)
+            controller.onDoneRequest(uri);
+        else
+            controller.onDone();
+    }
+}
