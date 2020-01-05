5f76aa5f943d07de907f04ca2bd808334ee55b72
reggiemcdonald <regmcdonald95@gmail.com>
2020-01-04 16:48:36 -0800

    Updated schema

--- neuralserv-api/src/main/resources/schema.sql
+++ neuralserv-api/src/main/resources/schema.sql
@@ -1,5 +1,6 @@
 DROP SEQUENCE IF EXISTS hibernate_sequence;
 DROP TABLE IF EXISTS number_image_entity, matrix_row_entity, training_session;
+
 CREATE TABLE number_image_entity (
     id bigint primary key,
     session_id bigint not null,
@@ -7,13 +8,6 @@ CREATE TABLE number_image_entity (
     expected_label integer,
     image_weights double precision[][]
 );
-CREATE TABLE matrix_row_entity (
-    id bigint primary key,
-    number_image_id bigint,
-    row_number integer not null,
-    row double precision[]
-);
-
 CREATE TABLE training_session (
     id serial primary key,
     internal_training_size integer not null,
@@ -22,8 +16,5 @@ CREATE TABLE training_session (
     external_number_correct integer not null,
     training_date timestamp not null
 );
-CREATE TABLE test (
-    id serial primary key,
-    value integer
-);
+
 CREATE SEQUENCE IF NOT EXISTS hibernate_sequence;
